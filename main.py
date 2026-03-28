41975a8864cb6df60cb880182d93a1e45888588b
Jidnyasa <jidnyasathakre123@gmail.com>
2026-03-28 22:04:09 +0530

    Initial commit - Regex Flask app

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import re
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    matches = []
+    error = ""
+
+    if request.method == "POST":
+        text = request.form.get("text")
+        pattern = request.form.get("pattern")
+
+        try:
+            matches = re.findall(pattern, text)
+        except:
+            error = "Invalid Regex Pattern!"
+
+    return render_template("index.html", matches=matches, error=error)
+
+
+if __name__ == "__main__":
+    app.run(port=5001, debug=False)
